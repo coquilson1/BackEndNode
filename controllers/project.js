03d10c475ec7ff948647f49402d603c9af87d9f1
coquilson1 <i.infante.dacruz@outlook.com>
2023-08-02 22:50:24 -0500

    get project byId

--- controllers/project.js
+++ controllers/project.js
@@ -1,6 +1,7 @@
 //Un controlador es una clase que va a tener una serie de métodos y acciones que van a estar relacionados con un modelo de datos específico 
 'use strict'
 
+const proyect = require('../models/proyect');
 //const proyect = require('../models/proyect'); /*** */
 var Project = require('../models/proyect'); //Tengo disponible mi modelo
 //const { param } = require('../routes/project'); /*** */
@@ -33,19 +34,6 @@ var controller = {
         project.langs = params.langs;
         project.image = null;
 
-        
-        /*Metodo del profesor que no me funcionó*/ 
-        /*
-        project.save( (err, projectStore) => {
-
-            if(err) return res.status(500).send({message: 'Error al guardar el documento'});
-
-            if(!projectStore) return res.status(404).send({message: 'No se ha podido guardar el proyecto'});
-
-           
-            return res.status(200).send({project: projectStore});
-        });*/
-
         project.save()
             .then(()=>{
                 res.status(200).send({project: project});
@@ -55,29 +43,20 @@ var controller = {
 
     }, 
 
-    getProject: function(req,res){
+    getProject: async function(req,res){
+
         var projectId = req.params.id;
 
-        if(projectId == null) return res.status(404).send({message: "El proyecto no existe"});
+        if(projectId == null) return res.status(404).send({message: "Debe ingresar código del proyecto"});
         
-
-        //Refiere que hay muchos métodos de este tipo y se podrían buscar en la documentación de Moongose
-        Project.findById(projectId, (err, project) => {
-            
-            if(err) return res.status(500).send({message: "Error al devolver los datos."});
-
-            if(!project) return res.status(404).send({message: "El proyecto no existe"});
-
-            return res.status(200).send({
-                project
-            })
-
-        });
-
+        try{
+            var project = await Project.findById(projectId)        
+            res.status(200).send({project:project});
+        }catch (err) {
+            res.status(500).send({message: "No se encuentra proyecto"});
+        }
+        
     }
-
-    
-
 
 };
 
--- routes/project.js
+++ routes/project.js
@@ -9,7 +9,7 @@ var router = express.Router();
 router.get('/home',ProjectController.home);
 router.post('/test',ProjectController.test);
 router.post('/save-project',ProjectController.saveProject); //http://localhost:3700/api/save-project //Body - x-www-form-urlencoded
-router.get('/project/:Id?', ProjectController.getProject) //opcional params 
+router.get('/project/:id?', ProjectController.getProject) //? : opcional params 
 
 module.exports = router;
 //obs a
